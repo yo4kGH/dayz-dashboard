933f2c72c4b86f1b7644de17d3bc7a838bcdb32b
yo4kGH <nathanholyoak@hotmail.com>
2025-11-29 05:23:49 +0000

    Fix config to JavaScript

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+module.exports = nextConfig;
